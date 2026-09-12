2e29d0a80a68a9d7d3bdda4cbd2508fbf73a0948
ZTZ888 <1242776548@qq.com>
2026-09-12 15:17:41 +0800

    tenth save(菜品分页查询)

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 
 /**
@@ -13,4 +15,10 @@ public interface DishService {
      * @param dishDTO
      */
     public void saveWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     */
+    PageResult page(DishPageQueryDTO dishPageQueryDTO);
 }
